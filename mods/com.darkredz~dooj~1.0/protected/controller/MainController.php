5a7b056e118782598d94d58caa70e5086984a4d7
Leng Sheng Hong <darkredz@gmail.com>
2013-11-25 18:10:07 +0000

    change example to use async mode

--- mods/com.darkredz~dooj~1.0/protected/controller/MainController.php
+++ mods/com.darkredz~dooj~1.0/protected/controller/MainController.php
@@ -6,10 +6,14 @@
  */
 class MainController extends DooController{
 
+	public $async = true;
+
     public function index(){
         $this->setContentType('html');
         $this->endReq('Hello! It works!');
-    }
 
+        //if aysnc mode is false
+        // echo "Hello! It works!"
+    }
 
 }
