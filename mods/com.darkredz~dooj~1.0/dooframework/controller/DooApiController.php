8cbfb5920fac60dbf9bba838360285a11be486cf
Leng Sheng Hong <leng@Lengs-MacBook-Air.local>
2018-04-09 23:34:10 +0800

    Added CORS support with fine grain options to allow certain headers/methods. Enable option to toggle auto input tracing in console for debugging. Allow api token and api key field to be customized. Auto delete uploaded temp files if api request is invalid. Decouple endpoint schema and validation so that it can be move to model or other places other than controller. Controllers are now all with the construct with app instance and container can be injected in.

--- mods/com.darkredz~dooj~1.0/dooframework/controller/DooApiController.php
+++ mods/com.darkredz~dooj~1.0/dooframework/controller/DooApiController.php
@@ -16,132 +16,356 @@
  * @package doo.controller
  * @since 2.0
  */
-class DooApiController extends DooController {
+class DooApiController extends DooController
+{
 
     public $lang = 'en_US';
     public $async = true;
     public $action;
     public $actionField;
     public $apiKey = 'AZ00B701J2pI90P84e7yrGhM401Z801';
-    public $fieldDef;
+    public $authHeaderTokenField = 'token';
+    public $authHeaderApiKeyField = 'apikey';
 
     public $authority;
+    public $enabledTraceInput = true;
+    public $allowCORS = false;
+    public $allowCORSCredentials = false;
+    public $allowCORSMethods = "GET,HEAD,OPTIONS,POST,PUT,DELETE";
+    public $allowCORSHeaders = "Origin,X-Requested-With,Content-Type,Accept,Authorization";
 
-    public function beforeRun($resource, $action){
+    /**
+     * Get RFC2617 Authorization formatted data. format is credentials = auth-scheme
+     * eg. Authorization: FIRE-TOKEN apikey=0PN5J17HBGZHT7JJ3X82, hash=frJIUN8DYpKDtOLCwo//yllqDzg
+     * @return array|null
+     */
+    protected function getAuthHeaderData()
+    {
+        $auth = $this->app->request->headers()->get('Authorization');
+        if (empty($auth)) {
+            return null;
+        }
+        $methodDataParts = \explode(' ', $auth, 2);
 
-        if($this->app->request->headers['Authority'] != $this->apiKey && $this->authority != $this->apiKey){
+        if (sizeof($methodDataParts) < 2) {
+            return null;
+        }
+
+        $authParts = \explode(',', $methodDataParts[1]);
+        $authData = [];
+
+        foreach ($authParts as $val) {
+            $keyAndVal = \explode('=', $val);
+            if (sizeof($keyAndVal) < 2) {
+                continue;
+            }
+            $authData[trim($keyAndVal[0])] = trim($keyAndVal[1]);
+        }
+
+        if (empty($authData)) {
+            $authData = $methodDataParts[1];
+        }
+
+        return [
+            'method' => $methodDataParts[0],
+            'data' => $authData,
+        ];
+    }
+
+    protected function getBearer()
+    {
+        $authData = $this->getAuthHeaderData();
+        if ($authData['method'] != 'Bearer') {
+            return null;
+        }
+        if (is_string($authData['data'])) {
+            return $authData['data'];
+        }
+        return $authData['data'];
+    }
+
+    protected function getBearerApiKey()
+    {
+        $authData = $this->getAuthHeaderData();
+        if ($authData['method'] != 'Bearer') {
+            return null;
+        }
+        if (is_string($authData['data'])) {
+            return $authData['data'];
+        }
+        return $authData['data'][$this->authHeaderApiKeyField];
+    }
+
+    protected function getBearerToken()
+    {
+        $authData = $this->getAuthHeaderData();
+        if ($authData['method'] != 'Bearer') {
+            return null;
+        }
+        if (is_string($authData['data'])) {
+            return $authData['data'];
+        }
+        return $authData['data'][$this->authHeaderTokenField];
+    }
+
+    public function beforeRun($resource, $action, $beforeRunHandler = null)
+    {
+        if (isset($this->app->conf->API_TRACE_ENABLED)) {
+            $this->enabledTraceInput = $this->app->conf->API_TRACE_ENABLED;
+        }
+        $bearerData = $this->getBearer();
+        $authOk = false;
+
+        if (is_string($bearerData) && $bearerData == $this->apiKey) {
+            $authOk = true;
+        } else {
+            if (is_array($bearerData) && $bearerData[$this->authHeaderApiKeyField] == $this->apiKey) {
+                $authOk = true;
+            }
+        }
+
+        $needApiKeyCheck = true;
+        //Resource => action No need to check API Key if inside exclude list, thus making it public
+        if (!empty($this->excludeApiKey) && \in_array($resource, \array_keys($this->excludeApiKey))) {
+            if (!empty($this->excludeApiKey[$resource])) {
+                $needApiKeyCheck = !\in_array($action, $this->excludeApiKey[$resource]);
+            } else {
+                $needApiKeyCheck = false;
+                $this->app->logDebug('NO NEED API KEY');
+            }
+        }
+
+        if ($this->app->_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            if ($this->allowCORS) {
+                $this->setHeader("Access-Control-Allow-Origin", ($this->allowCORS === true) ? '*' : $this->allowCORS);
+                $this->setHeader("Access-Control-Allow-Credentials", ($this->allowCORSCredentials) ? 'true' : 'false');
+                $this->setHeader("Access-Control-Allow-Methods", $this->allowCORSMethods);
+                $this->setHeader("Access-Control-Allow-Headers", $this->allowCORSHeaders);
+                $this->app->logDebug('CORS OPTIONS Pre flight');
+                $this->setContentType('json');
+                $this->app->statusCode = 200;
+                $this->endReq('{"response":"allow"}');
+                return 200;
+            } else {
+                $this->endReq('');
+            }
+        }
+
+        if ($needApiKeyCheck && !$authOk) {
             $this->setContentType('json');
             $this->app->statusCode = 401;
-            $this->endReq( '{"error":"Invalid API key"}' );
+            $this->endReq('{"error":"Invalid API key"}');
             return 401;
         }
 
         $this->initReqAction($action);
 
-        if($this->isMethodAllow() == false){
+        if ($this->isMethodAllow() == false) {
             $this->setContentType("json");
             $this->app->statusCode = 404;
-            $this->endReq( '{"error":"Method Not Found"}' );
+            $this->endReq('{"error":"Method Not Found"}');
             return;
         }
     }
 
-    protected function initReqAction($action){
+    protected function initReqAction($action)
+    {
         $this->action = $action;
         $this->actionField = 'field' . ucfirst($this->action);
     }
 
-    protected function isMethodAllow(){
+    protected function isMethodAllow()
+    {
         $allowMethod = $this->{$this->actionField}['_method'];
 
-        if($allowMethod && strtoupper($allowMethod) != $this->app->_SERVER['REQUEST_METHOD']){
+        if ($allowMethod && strtoupper($allowMethod) != $this->app->_SERVER['REQUEST_METHOD']) {
             return false;
         }
         return true;
     }
 
-    protected function getFieldSchema(){
-        if($this->actionField==null) return;
+    protected function getFieldSchema()
+    {
+        if ($this->actionField == null) {
+            return;
+        }
         return $this->{$this->actionField};
     }
 
-    protected function getFieldRules(){
+    protected function getApiInput()
+    {
+        $field = array_keys($this->{$this->actionField});
+        $method = $this->app->_SERVER['REQUEST_METHOD'];
+
+        if ($method == 'GET') {
+            $input = [];
+            if (!empty($this->_GET)) {
+                $input = $this->getInput($field, $this->_GET);
+            }
+            if (!empty($this->params)) {
+                //convert to use field underscore _ if found key is using -
+                $input1 = $this->getKeyParamsDashToUnderscore($field, true);
+                $input = array_merge($input, $input1);
+            }
+        } else {
+            if ($method == 'OPTIONS' || $method == 'HEAD') {
+                $input = $this->getKeyParams($field, true);
+            } else {
+                $input = $this->getInput($field, $this->_POST);
+                if (!empty($this->_FILES)) {
+                    $allFields = $this->{$this->actionField};
+                    $uploadFields = [];
+                    foreach ($allFields as $key => $schema) {
+                        if ($schema[0] == 'file') {
+                            $uploadFields[] = $key;
+                        }
+                    }
+                    $inputUpload = $this->getInput($uploadFields, $this->_FILES);
+                    $input = array_merge($input, $inputUpload);
+                }
+            }
+        }
+
+        unset($input['_method']);
+        unset($input['_return_type']);
+
+        if ($this->app->conf->DEBUG_ENABLED) {
+            if ($this->enabledTraceInput) {
+                $this->app->logDebug($method . ' Request input:');
+                $this->app->trace($input);
+            }
+        }
+
+        return $input;
+    }
+
+    protected function deleteUploadTmpFiles()
+    {
+        //on error, if file has error remove it.
+        foreach ($this->_FILES as $f) {
+            $tmpName = $f['tmp_name'];
+            $this->app->vertx->setTimer(100, g(function ($tid) use ($tmpName) {
+                $this->app->vertx->fileSystem()->exists($tmpName, a(function ($result, $error) use ($tmpName) {
+                    if ($result) {
+                        $this->app->vertx->fileSystem()->delete($tmpName, null);
+                    }
+                }));
+            }));
+        }
+    }
+
+    protected function getFieldRules($validationFields = null)
+    {
         $rules = [];
-        $field = $this->{$this->actionField};
-        foreach($field as $fname => $p){
-            if($fname == '_method') continue;
-            if(isset($p[1])){
+        if (!empty($validationFields)) {
+            if (is_array($validationFields)) {
+                $fieldRules = $validationFields[$this->actionField];
+            } else {
+                if (is_object($validationFields)) {
+                    $fieldRules = $validationFields->{$this->actionField};
+                } else {
+                    if (is_string($validationFields)) {
+                        $validationFieldObj = \JSON::decode($validationFields);
+                        $fieldRules = $validationFieldObj->{$this->actionField};
+                    }
+                }
+            }
+        } else {
+            $fieldRules = $this->{$this->actionField};
+        }
+
+        foreach ($fieldRules as $fname => $p) {
+            if ($fname == '_method') {
+                continue;
+            }
+            if (isset($p[1])) {
                 $rules[$fname] = $p[1];
             }
         }
         return $rules;
     }
 
-    protected function getApiInput(){
-        $field = array_keys($this->{$this->actionField});
-        $method = $this->app->_SERVER['REQUEST_METHOD'];
-
-        if($method == 'GET' || $method == 'OPTIONS' || $method == 'HEAD'){
-            $input = $this->getKeyParams($field);
+    /**
+     * @param DooInputValidator $validateRes
+     * @return bool|array
+     */
+    protected function validationCleanup($validateRes)
+    {
+        if ($validateRes->resultType === \DooInputValidatorResult::VALID) {
+            return $validateRes->inputValues;
         }
-        else{
-            $input = $this->getInput($field, $this->_POST);
+        else if ($validateRes->resultType === \DooInputValidatorResult::VALID) {
+            return $validateRes->inputValues;
         }
-
-        if($this->app->conf->DEBUG_ENABLED){
-            $this->app->logInfo($method . ' Request input:');
-            $this->app->trace($input);
-        }
-
-        return $input;
-    }
-
-    protected function validateInput(){
-        $input = $this->getApiInput();
-        $rules = $this->getFieldRules();
-
-        if($input==null){
+        else if ($validateRes->resultType === \DooInputValidatorResult::INVALID_NO_INPUT) {
+            $this->deleteUploadTmpFiles();
             $this->sendError('No data input');
             return false;
         }
-
-        if($rules==null){
+        else if ($validateRes->resultType === \DooInputValidatorResult::INVALID_NO_RULES) {
+            $this->deleteUploadTmpFiles();
             $this->sendError('Please set the field rules for this API action');
             return false;
         }
-
-        $v = new DooValidator();
-        $v->checkMode = DooValidator::CHECK_ALL;
-        $err = $v->validate($input, $rules);
-
-        if($err){
-            $this->sendError($err);
+        else if ($validateRes->resultType === \DooInputValidatorResult::INVALID_RULE_ERRORS) {
+            $this->deleteUploadTmpFiles();
+            $this->sendError($validateRes->errors);
             return false;
         }
-
-        return $input;
     }
 
-    protected function removeFields($opt, $removeTheseFields=null, $removeNull=true, $removeEmptyString=true){
+    protected function validateInput($input = null)
+    {
+        if (is_null($input)) {
+            $input = $this->getApiInput();
+        }
+        $rules = $this->{$this->actionField};
+
+        $inputValidator = new \DooInputValidator($rules);
+        $validateRes = $inputValidator->validateInput($input);
+
+        return $this->validationCleanup($validateRes);
+    }
+
+    protected function setupServiceValidationHook($service)
+    {
+        $controller = &$this;
+        $validateHook = function($serviceName, $serviceProvider, $params) use ($controller) {
+            $input = $params[0];
+            $rules = $controller->{$controller->actionField};
+
+            $controllerCleanupCallback = function($validateRes) use ($controller) {
+                return $controller->validationCleanup($validateRes);
+            };
+
+            return $serviceProvider->validateInput($input, $rules, $controllerCleanupCallback);
+        };
+
+        $service->setPreCallHook($validateHook);
+        return $service;
+    }
+
+    protected function removeFields($opt, $removeTheseFields = null, $removeNull = true, $removeEmptyString = true)
+    {
         //remove fields that are not needed for where query
-        if($removeTheseFields){
+        if ($removeTheseFields) {
             $optFields = array_keys($opt);
 
-            foreach ($removeTheseFields as $field){
-                if(in_array($field, $optFields)){
+            foreach ($removeTheseFields as $field) {
+                if (in_array($field, $optFields)) {
                     unset($opt[$field]);
                 }
             }
         }
 
         //remove null fields
-        if($removeNull || $removeEmpty){
-            foreach($opt as $k=>$v){
-                if($removeNull && $v===null) {
+        if ($removeNull || $removeEmptyString) {
+            foreach ($opt as $k => $v) {
+                if ($removeNull && $v === null) {
                     unset($opt[$k]);
                     continue;
                 }
-                if($removeEmptyString && $v===''){
+                if ($removeEmptyString && $v === '') {
                     unset($opt[$k]);
                     continue;
                 }
@@ -150,23 +374,57 @@ class DooApiController extends DooController {
         return $opt;
     }
 
-    protected function sendError($err, $statusCode=400){
+    public function endReq($out = null)
+    {
+        if ($this->allowCORS) {
+            $this->setHeader("Access-Control-Allow-Origin", ($this->allowCORS === true) ? '*' : $this->allowCORS);
+            $this->setHeader("Access-Control-Allow-Credentials", ($this->allowCORSCredentials) ? 'true' : 'false');
+        }
+        parent::endReq($out);
+    }
+
+    protected function sendError($err, $statusCode = 400)
+    {
         $this->setContentType('json');
         $this->app->statusCode = $statusCode;
-        if(is_string($err)){
-            $this->endReq( $err );
-        }else{
-            $this->endReq( \JSON::encode($err) );
+        if (is_string($err)) {
+            $this->endReq($err);
+        } else {
+            $this->endReq(\JSON::encode($err));
         }
     }
 
-    protected function sendResult($jsonStr, $statusCode=200){
+    protected function sendResult($jsonStr, $statusCode = 200)
+    {
         $this->setContentType('json');
         $this->app->statusCode = $statusCode;
-        if(is_string($jsonStr)){
-            $this->endReq( $jsonStr );
-        }else{
-            $this->endReq( \JSON::encode($jsonStr) );
+        if (is_string($jsonStr)) {
+            $this->endReq($jsonStr);
+        } else {
+            $this->endReq(\JSON::encode($jsonStr));
+        }
+    }
+
+
+    protected function getEndpointSchema($endpointName)
+    {
+        $filepath = $this->app->conf->SITE_PATH . $this->app->conf->PROTECTED_FOLDER . 'endpoint/' . $endpointName . '.php';
+        return include($filepath);
+    }
+
+    protected function prepareAllEndpointSchema()
+    {
+        $reflect = new \ReflectionClass($this);
+        $clsName = \lcfirst(\substr($reflect->getShortName(), 0, -10));
+        $props = $reflect->getProperties();
+
+        foreach ($props as $prop) {
+            $propName = $prop->getName();
+
+            if (\is_null($this->{$propName}) && \strpos($propName, 'field') === 0) {
+                $methodName = \lcfirst(\substr($propName, 5));
+                $this->{$propName} = $this->getEndpointSchema($clsName . '/' . $methodName);
+            }
         }
     }
 }
