ab87ddeaf6d0b9d6f82632116e7507d9c87f30f6
Leng Sheng Hong <leng@Lengs-MacBook-Air.local>
2018-04-10 14:44:51 +0800

    updated mock class and example web app mock

--- mods/com.darkredz~dooj~1.0/dooframework/ext/ArrBDD/ArrMock.php
+++ mods/com.darkredz~dooj~1.0/dooframework/ext/ArrBDD/ArrMock.php
@@ -15,8 +15,9 @@
  * @author Leng Sheng Hong <darkredz@gmail.com>
  * @since 0.1
  */
-class ArrMock {
-    public $methods = array();    
+class ArrMock
+{
+    public $methods = [];
     protected $toStringVal;
     protected $lastMethod;
     protected $lastStaticMethod;
@@ -27,12 +28,16 @@ class ArrMock {
     /**
      * Prevent direct object creation of ArrMock
      */
-    final private function  __construct(){}
+    final private function __construct()
+    {
+    }
 
     /**
      * Prevent object cloning of ArrMock
      */
-    final private function  __clone(){}
+    final private function __clone()
+    {
+    }
 
     /**
      * This must be the first call to create the Mock Object
@@ -41,17 +46,18 @@ class ArrMock {
      * @return ArrMock
      * @throws Exception
      */
-    public static function create( $className = 'AnonClass', $staticAttr = null ){
-        if(empty($className) || !is_string($className)){
+    public static function create($className = 'AnonClass', $staticAttr = null)
+    {
+        if (empty($className) || !is_string($className)) {
             throw new Exception('Class name of the mock object is required');
         }
-        
+
         $className .= 'Mock';
-        
+
         $staticAttrStr = '';
-        if(is_array($staticAttr)){
-            foreach( $staticAttr as $attr ){
-                $staticAttrStr .= 'public static $' . $attr .';';
+        if (is_array($staticAttr)) {
+            foreach ($staticAttr as $attr) {
+                $staticAttrStr .= 'public static $' . $attr . ';';
             }
         }
 
@@ -150,165 +156,184 @@ EOF
         );
         return new $className;
     }
-    
-    public function method( $name ){
+
+    public function method($name)
+    {
         $this->lastMethod = $name;
         $this->lastStaticMethod = $this->lastArgs = null;   //reset
         return $this;
     }
-    
-    public function staticMethod( $name ){
+
+    public function staticMethod($name)
+    {
         $this->lastStaticMethod = $name;
-        $this->lastMethod = $this->lastArgs = null;   //reset   
+        $this->lastMethod = $this->lastArgs = null;   //reset
         return $this;
     }
 
-    public function args(){
-        if( isset($this->lastMethod) || isset($this->lastStaticMethod) ){
+    public function args()
+    {
+        if (isset($this->lastMethod) || isset($this->lastStaticMethod)) {
             $args = func_get_args();
             $this->lastArgs = $this->serializeArgs($args);
         }
         return $this;
     }
 
-    public function handle($func){            
-        if(isset($this->lastMethod)){
+    public function handle($func)
+    {
+        if (isset($this->lastMethod)) {
             $this->methods[$this->lastMethod]['handler'] = $func;
+        } else {
+            if (isset($this->lastStaticMethod)) {
+                return $func;
+            }
         }
-        else if(isset($this->lastStaticMethod)){  
-            return $func;
-        }
-        return $this;    
+        return $this;
     }
 
-    public function autoChain(){
-        $this->handle(function($args){
+    public function autoChain()
+    {
+        $this->handle(function ($args) {
             return $this;
         });
     }
-    
-    public function returns( $returnVal = null, $position = null, $execFunc = null){
-        if( isset($this->lastMethod) || isset($this->lastStaticMethod) ){    
-            if( $this->lastArgs===null ){
+
+    public function returns($returnVal = null, $position = null, $execFunc = null)
+    {
+        if (isset($this->lastMethod) || isset($this->lastStaticMethod)) {
+            if ($this->lastArgs === null) {
                 $this->lastArgs = $this->serializeArgs([]);
             }
 
-            if(isset($this->lastMethod)){
-            
-                if(empty($this->methods[$this->lastMethod][$this->lastArgs])){
-                    $this->methods[$this->lastMethod][$this->lastArgs] = array();
-                    $this->methods[$this->lastMethod][$this->lastArgs]['returns'] = array();
+            if (isset($this->lastMethod)) {
+
+                if (empty($this->methods[$this->lastMethod][$this->lastArgs])) {
+                    $this->methods[$this->lastMethod][$this->lastArgs] = [];
+                    $this->methods[$this->lastMethod][$this->lastArgs]['returns'] = [];
                 }
-                
+
                 $ret = &$this->methods[$this->lastMethod][$this->lastArgs]['returns'];
                 self::getReturnVal($ret, $returnVal, $position);
-            }
-            else{
+            } else {
                 return $returnVal;
             }
         }
-        
+
         return $this;
     }
-    
-    protected static function getReturnVal(&$ret, $returnVal, $position){                
-        if( is_int($position) ){
+
+    protected static function getReturnVal(&$ret, $returnVal, $position)
+    {
+        if (is_int($position)) {
             $retLength = sizeof($ret);
-            
-            if( $retLength==0 ){
+
+            if ($retLength == 0) {
                 $ret[] = $returnVal;
                 $retLength++;
             }
-            
-            $prevVal = $ret[ $retLength-1 ];
+
+            $prevVal = $ret[$retLength - 1];
             $position = $position - 1 - $retLength;
-            
-            if( $position > 0 ){
+
+            if ($position > 0) {
                 $fill = array_fill($retLength, $position, $prevVal);
                 $ret = array_merge($ret, $fill);
                 //echo "$retLength, $position, $prevVal";
                 //var_dump( $fill );
             }
             $ret[] = $returnVal;
-        }
-        else{
+        } else {
             $ret[] = $returnVal;
-        }    
-    }
-        
-    public function __toString() {
-        if(isset($this->toStringVal))
-            return $this->toStringVal;
+        }
     }
 
-    public function __call($name, $arguments) {
-        if( isset( $this->methods[$name] ) ){
+    public function __toString()
+    {
+        if (isset($this->toStringVal)) {
+            return $this->toStringVal;
+        }
+    }
+
+    public function __call($name, $arguments)
+    {
+        if (isset($this->methods[$name])) {
 
             $args = $this->serializeArgs($arguments);
 
-            if( isset( $this->methods[$name][$args] ) ){
+            if (isset($this->methods[$name][$args])) {
                 $ret = &$this->methods[$name][$args];
 
-                if( !isset($ret['count']) )
+                if (!isset($ret['count'])) {
                     $ret['count'] = 0;
+                }
 
                 $count = $ret['count']++;
                 $retLength = sizeof($ret['returns']);
 
-                if( $count >= $retLength )
+                if ($count >= $retLength) {
                     $count = $retLength - 1;
+                }
 
-                return $ret['returns'][ $count ];
-            }
-            else if( !empty($this->methods[$name]['handler']) ){
-                if( !isset($this->methods[$name]['handlerCallCount']) )
-                    $this->methods[$name]['handlerCallCount'] = 0;
-                $this->methods[$name]['handlerCallCount']++;
-                return $this->methods[$name]['handler']($arguments);
+                return $ret['returns'][$count];
+            } else {
+                if (!empty($this->methods[$name]['handler'])) {
+                    if (!isset($this->methods[$name]['handlerCallCount'])) {
+                        $this->methods[$name]['handlerCallCount'] = 0;
+                    }
+                    $this->methods[$name]['handlerCallCount']++;
+                    return $this->methods[$name]['handler']($arguments);
+                }
             }
         }
 
-        if($this->ignoreNonExistentMethod!=true){
-            if($this->varDumpException){
-                var_dump("Call to undefined method ". $this->selfClassName ."::$name()");
-            }
-            else{
-                throw new Exception("Call to undefined method ". $this->selfClassName ."::$name()");
+        if ($this->ignoreNonExistentMethod != true) {
+            if ($this->varDumpException) {
+                var_dump("Call to undefined method " . $this->selfClassName . "::$name()");
+            } else {
+                throw new Exception("Call to undefined method " . $this->selfClassName . "::$name()");
             }
         }
     }
-    
-    protected function calcTotalCalls( $methodsList, $method, $args ){
+
+    protected function calcTotalCalls($methodsList, $method, $args)
+    {
         // get total method calls on both static and non-static coz they can't have the same name
-        if( !empty($methodsList[$method]) ){
+        if (!empty($methodsList[$method])) {
             $methodsCalled = $methodsList[$method];
             $total = 0;
-            
-            $emptyArgs = (sizeof($args)===0);
+
+            $emptyArgs = (sizeof($args) === 0);
             //null should be converted to an empty array to be var_export as key
-            if( sizeof($args)===1 && $args[0]===null ) 
-                $args = array();
-            
-            if(!$emptyArgs){
+            if (sizeof($args) === 1 && $args[0] === null) {
+                $args = [];
+            }
+
+            if (!$emptyArgs) {
                 $args = $this->serializeArgs($args);
             }
 
             // add up all methods call count
-            foreach($methodsCalled as $argsKey => $mc){
-                if($argsKey==='handlerCallCount') continue;
-                
-                if($argsKey==='handler'){
-                    if($emptyArgs && isset($methodsCalled['handlerCallCount']))
-                        $total += $methodsCalled['handlerCallCount'];
+            foreach ($methodsCalled as $argsKey => $mc) {
+                if ($argsKey === 'handlerCallCount') {
+                    continue;
                 }
-                else if(isset($mc['count'])){
-                    // all methods, ignore matching args
-                    if( $emptyArgs ){
-                        $total += $mc['count'];
+
+                if ($argsKey === 'handler') {
+                    if ($emptyArgs && isset($methodsCalled['handlerCallCount'])) {
+                        $total += $methodsCalled['handlerCallCount'];
                     }
-                    // try matching the arguments as key
-                    else if( $argsKey===$args ){
-                        $total += $mc['count']; 
+                } else {
+                    if (isset($mc['count'])) {
+                        // all methods, ignore matching args
+                        if ($emptyArgs) {
+                            $total += $mc['count'];
+                        } // try matching the arguments as key
+                        else {
+                            if ($argsKey === $args) {
+                                $total += $mc['count'];
+                            }
+                        }
                     }
                 }
             }
@@ -316,29 +341,30 @@ EOF
         }
     }
 
-    public function serializeArgs($args){
+    public function serializeArgs($args)
+    {
         return self::_serializeArgs($args);
     }
 
-    public static function _serializeArgs($args){
+    public static function _serializeArgs($args)
+    {
         $closure = false;
         $clsArgs = [];
         //convert closures into string, var_export not working for closures
-        foreach($args as $k=>$v){
-            if(is_callable($v)){
+        foreach ($args as $k => $v) {
+            if (is_callable($v)) {
                 $closure = true;
                 ob_start();
                 var_dump($v);
                 $clousureExp = ob_get_clean();
                 $clsArgs[$k] = $clousureExp;
                 break;
-            }
-            else{
+            } else {
                 $clsArgs[$k] = $v;
             }
         }
 
-        if($closure){
+        if ($closure) {
             return var_export($clsArgs, true);
         }
         return var_export($args, true);
